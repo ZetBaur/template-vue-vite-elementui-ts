c037bbe86e50552d660c10b6973fba4b405d565b
miloserdov <smiloserdov@citrobyte.com>
2022-07-18 00:31:34 +0600

    next

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>This is the Home page</main>
 </template>
+
+<script setup lang="ts"></script>

8b0cb5d48957b5acf57ab96bfdd2247c373306ad
miloserdov <smiloserdov@citrobyte.com>
2022-07-18 00:02:22 +0600

    next

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,37 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-    <header>
-        <img
-            alt="Vue logo"
-            class="logo"
-            src="@/assets/logo.svg"
-            width="125"
-            height="125"
-        />
-
-        <div class="wrapper">
-            <HelloWorld msg="You did it!" />
-
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
-    </header>
-
-    <RouterView />
+    <component :is="layout + '-layout'" v-if="layout" />
 </template>
 
+<script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import MainLayout from './layouts/MainLayout.vue';
+import AuthLayout from './layouts/AuthLayout.vue';
+
+export default {
+    setup() {
+        const route = useRoute();
+        const layout = computed(() => route.meta.layout);
+
+        return {
+            layout
+        };
+    },
+    components: { MainLayout, AuthLayout }
+};
+</script>
+
 <style lang="scss">
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-
-    h3 {
-        color: red;
-    }
+html,
+body {
+    margin: 0px;
+    min-height: 100%;
+    height: 100%;
 }
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-nav {
+#app {
+    height: 100%;
     width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
+    min-height: 100%;
 }
 </style>
---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="auth">
+        <router-view />
+    </div>
+</template>
+
+<style setup lang="scss">
+.auth {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
+
+
